b51ccfd492f43dd3341a6e8c3869860880b0c669
orionlabs <admin@orionlabs.in>
2023-11-23 12:16:41 +0530

    changed db

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ const Pool = require('pg').Pool
 
 const pool = new Pool({
     // connectionString: `postgres://neworioncrm_user:kAraeqt8CXHp6IrEFiFpcFYWOOVWAWt9@dpg-ck59g0mg2bec73fhpt7g-a.oregon-postgres.render.com/neworioncrm `
-    user: 'neworioncrm_user',
-    host: 'dpg-ck59g0mg2bec73fhpt7g-a.oregon-postgres.render.com',
-    database: 'neworioncrm',
-    password: 'kAraeqt8CXHp6IrEFiFpcFYWOOVWAWt9',
+    user: 'orionlabs',
+    host: 'dpg-cletctd3qkas73b0c1v0-a.oregon-postgres.render.com',
+    database: 'oriondcrmdb',
+    password: 'bnfxq8IyaUjwG0pSn6id0oXB5T9LYHtT',
     port: 5432,
     ssl: true
 })
